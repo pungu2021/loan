9fbeb3875d37bd65b643c56e3c96153b6f9aa35d
carlos pungu <pungucarlos@gmail.com>
2024-04-26 12:10:49 +0100

    my first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/styleLoan.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Loan</title>
+</head>
+<body>
+    <main>
+        <header>
+              <span class="advans">ADVANS CONGO</span>
+              <div class="heure">{{date("d-m-Y")}}</div>
+        </header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-8">
+                    <img src="Images/loa.jpg" alt="" class="images-loan">
+                </div>
+                <div class="col-lg-4 mt-5">
+                    <div class="div-adv">
+                       ADVANS CONGO LOAN
+                    </div>
+                    <div class="seconnecte">Je me connecte avec mon compte</div>
+                    <form action="" method="POST" class="form-image">
+                        <label for="au">Email advans congo</label>
+                        <input type="text" name="gmail" id="" class="form-control">
+                        <label for="au">Mot de passe</label>
+                        <input type="text" name="password" id="" class="form-control">
+                        <input type="checkbox" name="" id=""> j'accept le condition des utilisations <br>
+                        <a href="">Mot de passe oublié</a><br>
+                        <button class="btn-adv mt-2">Connexion</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+    <script src="js/time.js"></script>
+</body>
+</html>
